c1e147a913d68a6752a80c62e048f0219e027ce3
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-09-18 15:27:42 +0200

    chore(api): add Joi validation schema for environment variables

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,16 +2,20 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CqrsModule } from '@nestjs/cqrs';
 import { AdminGatewayModule } from './bff/admin-gateway/admin-gateway.module';
+import { envSchema } from '../env-schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: envSchema,
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
     }),
     CqrsModule.forRoot(),
     AdminGatewayModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
