5c698af76af2ba3b4cff9178e36ff1d74243a285
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-10-06 14:38:34 +0200

    chore(api): add request reset password endpoint with DTO and controller method

---
+++ apps/api/src/bff/admin-gateway/dtos/admin-request-reset-password.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class AdminRequestResetPasswordDto {
+  @ApiProperty({
+    description: 'Email',
+    format: 'email',
+  })
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+}
