ffeeab6650395426e17c17710f20f5519b683483
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-10-08 13:54:36 +0200

    chore(admin): add "Validate Reset Password Token" functionality with query and handler integration

---
+++ apps/api/src/admin-identity/application/query-handlers/validate-reset-password-token.query-handler.ts
@@ -0,0 +1,40 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { ValidateResetPasswordTokenQuery } from '../queries/validate-reset-password-token.query';
+import { PrismaService } from '../../../common/prisma/prisma.service';
+import { ResetPasswordTokensStorage } from '../ports/reset-password-tokens.storage';
+
+export type ValidateResetPasswordTokenQueryResponse = { valid: boolean };
+
+@QueryHandler(ValidateResetPasswordTokenQuery)
+export class ValidateResetPasswordTokenQueryHandler
+  implements
+    IQueryHandler<
+      ValidateResetPasswordTokenQuery,
+      ValidateResetPasswordTokenQueryResponse
+    >
+{
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly resetTokensStorage: ResetPasswordTokensStorage,
+  ) {}
+
+  async execute(
+    query: ValidateResetPasswordTokenQuery,
+  ): Promise<ValidateResetPasswordTokenQueryResponse> {
+    const { token } = query;
+
+    const adminUser = await this.prismaService.adminUser.findFirst({
+      where: {
+        resetPasswordToken: token,
+      },
+    });
+
+    if (!adminUser) return { valid: false };
+
+    const isTokenValid = await this.resetTokensStorage.validate(adminUser.id);
+
+    if (!isTokenValid) return { valid: false };
+
+    return { valid: true };
+  }
+}
