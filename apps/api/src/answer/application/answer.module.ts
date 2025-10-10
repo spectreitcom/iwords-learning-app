c4e32057e3804a361120ae9fe5b67cc246f8ab2e
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-10-10 11:45:35 +0200

    chore(api): add DictionaryExpressionContextUpdatedEventHandler

--- apps/api/src/answer/application/answer.module.ts
+++ apps/api/src/answer/application/answer.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../common/prisma/prisma.module';
 import { DictionaryExpressionContextCreatedEventHandler } from './event-handlers/dictionary-expression-context-created.event-handler';
 import { DictionarySentenceCreatedEventHandler } from './event-handlers/dictionary-sentence-created.event-handler';
+import { DictionaryExpressionContextUpdatedEventHandler } from './event-handlers/dictionary-expression-context-updated.event-handler';
 
 const EVENT_HANDLERS = [
   DictionaryExpressionContextCreatedEventHandler,
   DictionarySentenceCreatedEventHandler,
+  DictionaryExpressionContextUpdatedEventHandler,
 ];
 
 @Module({
---
+++ apps/api/src/answer/application/event-handlers/dictionary-expression-context-updated.event-handler.ts
@@ -0,0 +1,68 @@
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
+import { IntegrationEvent } from '../../../common/outbox/types';
+import { Logger } from '@nestjs/common';
+import { PrismaService } from '../../../common/prisma/prisma.service';
+
+type EventPayload = {
+  expressionContextId: string;
+  expressionId: string;
+  phrase: string;
+  type: string;
+  translation: string;
+  forms: [string, string, string] | null;
+  isIrregular: boolean;
+  isCountable: boolean;
+};
+
+@EventsHandler(IntegrationEvent)
+export class DictionaryExpressionContextUpdatedEventHandler
+  implements IEventHandler<IntegrationEvent<EventPayload>>
+{
+  private readonly logger = new Logger(
+    DictionaryExpressionContextUpdatedEventHandler.name,
+  );
+
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async handle(event: IntegrationEvent<EventPayload>) {
+    if (event.type !== 'dictionary.expression-context-updated') return null;
+
+    this.logger.debug(JSON.stringify(event));
+
+    const {
+      expressionId,
+      expressionContextId,
+      isCountable,
+      isIrregular,
+      phrase,
+      translation,
+      forms,
+      type,
+    } = event.payload;
+
+    const record =
+      await this.prismaService.answerExpressionContextReadModel.findUnique({
+        where: {
+          expressionContextId,
+          expressionId,
+        },
+      });
+
+    if (!record) return null;
+
+    await this.prismaService.answerExpressionContextReadModel.update({
+      where: {
+        expressionContextId,
+        expressionId,
+      },
+      data: {
+        isCountable,
+        isIrregular,
+        phrase,
+        translation,
+        forms: forms ? forms : [],
+        type,
+      },
+    });
+  }
+}
