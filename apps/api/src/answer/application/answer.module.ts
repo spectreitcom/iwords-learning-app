89c86d5c786b6550faa8436040ebe8001788c240
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-10-10 11:39:07 +0200

    chore(api): add DictionarySentenceCreatedEventHandler to handle "dictionary.sentence-created" events

--- apps/api/src/answer/application/answer.module.ts
+++ apps/api/src/answer/application/answer.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../common/prisma/prisma.module';
 import { DictionaryExpressionContextCreatedEventHandler } from './event-handlers/dictionary-expression-context-created.event-handler';
+import { DictionarySentenceCreatedEventHandler } from './event-handlers/dictionary-sentence-created.event-handler';
 
-const EVENT_HANDLERS = [DictionaryExpressionContextCreatedEventHandler];
+const EVENT_HANDLERS = [
+  DictionaryExpressionContextCreatedEventHandler,
+  DictionarySentenceCreatedEventHandler,
+];
 
 @Module({
   imports: [PrismaModule],
