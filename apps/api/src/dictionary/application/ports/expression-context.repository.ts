f3c599d395472b8c1758e050c2495df848baa600
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-10-10 12:59:05 +0200

    chore(api): add transactional support to ExpressionContextRepository and integrate outbox event

--- apps/api/src/dictionary/application/ports/expression-context.repository.ts
+++ apps/api/src/dictionary/application/ports/expression-context.repository.ts
@@ -1,11 +1,15 @@
 import { ExpressionContext } from '../../domain/expression-context';
+import { PrismaTx } from '../../../common/types';
 
 export abstract class ExpressionContextRepository {
-  abstract save(expressionContext: ExpressionContext): Promise<void>;
+  abstract save(
+    expressionContext: ExpressionContext,
+    tx?: PrismaTx,
+  ): Promise<void>;
   abstract findById(
     expressionContextId: string,
   ): Promise<ExpressionContext | null>;
-  abstract delete(expressionContextId: string): Promise<void>;
+  abstract delete(expressionContextId: string, tx?: PrismaTx): Promise<void>;
   abstract findByIdAndType(
     expressionContextId: string,
     type: string,
