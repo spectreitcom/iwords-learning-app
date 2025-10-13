69920c3d7516c195efb129dd6dc53859e957a0d8
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-10-13 12:45:10 +0200

    chore(admin): implement users list table with actions for blocking and unblocking users

--- apps/admin/src/app/(app)/users/page.tsx
+++ apps/admin/src/app/(app)/users/page.tsx
@@ -1,3 +1,109 @@
-export default function UsersPage() {
-  return null;
+import { Suspense } from "react";
+import { getUsers } from "@/features/users/actions";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { User } from "@/features/users/types";
+import { Pagination } from "@/components/pagination";
+import { NoDataPlaceholder } from "@/components/no-data-placeholder";
+import { TableSkeletonLoader } from "@/components/table-skeleton-loader";
+import { UsersTableItemActions } from "@/features/users/components/users-table-item-actions";
+import { Badge } from "@/components/ui/badge";
+
+const TAKE = 20;
+
+type Props = {
+  searchParams: Promise<{
+    page: string;
+  }>;
+};
+
+export default async function UsersPage({ searchParams }: Props) {
+  const searchParamsValues = await searchParams;
+  return (
+    <div>
+      <h1 className={"text-2xl"}>Lista użytkowników</h1>
+      <div className={"mt-8"}>
+        <Suspense
+          fallback={
+            <TableSkeletonLoader
+              headers={["Email", "Imię i nazwisko", "Status", ""]}
+              showPagination={true}
+            />
+          }
+        >
+          <AwaitedContent searchParamsValues={searchParamsValues} />
+        </Suspense>
+      </div>
+    </div>
+  );
+}
+
+async function AwaitedContent({
+  searchParamsValues,
+}: {
+  searchParamsValues: { page: string };
+}) {
+  const page = searchParamsValues.page ? parseInt(searchParamsValues.page) : 1;
+  const usersData = await getUsers(page, TAKE);
+  if (!usersData.data?.length)
+    return (
+      <div className={"mt-8"}>
+        <NoDataPlaceholder
+          heading="Brak użytkowników"
+          description="Nie znaleziono żadnych użytkowników w bazie danych."
+        />
+      </div>
+    );
+  return (
+    <div>
+      <div className={"mt-4"}>
+        <UsersListTable users={usersData.data} />
+      </div>
+      <div className={"flex justify-end mt-4"}>
+        <Pagination
+          currentPage={usersData.currentPage}
+          total={usersData.total}
+          take={TAKE}
+          otherSearchParams={searchParamsValues}
+        />
+      </div>
+    </div>
+  );
+}
+
+function UsersListTable({ users }: { users: User[] }) {
+  return (
+    <Table className={"w-full"}>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Email</TableHead>
+          <TableHead>Imię i nazwisko</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead />
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {users.map((user) => (
+          <TableRow key={user.userId}>
+            <TableCell>{user.email}</TableCell>
+            <TableCell>{user.name}</TableCell>
+            <TableCell>
+              <Badge variant={user.blocked ? "destructive" : "secondary"}>
+                {user.blocked ? "Zablokowany" : "Aktywny"}
+              </Badge>
+            </TableCell>
+            <TableCell className={"flex justify-end"}>
+              <UsersTableItemActions user={user} />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
