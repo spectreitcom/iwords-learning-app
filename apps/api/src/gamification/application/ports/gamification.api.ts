b42a87426032fbd938e17d9bf538e4b420ea7132
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-10-30 10:08:40 +0100

    chore(api): add `getUserTodayPoints` query, handler, and integration into `GamificationModule`

--- apps/api/src/gamification/application/ports/gamification.api.ts
+++ apps/api/src/gamification/application/ports/gamification.api.ts
@@ -1,4 +1,5 @@
 export abstract class GamificationApi {
   abstract setUpDailyGoal(userId: string, goal: number): Promise<void>;
   abstract getUserDailyGoal(userId: string): Promise<number>;
+  abstract getUserTodayPoints(userId: string): Promise<number>;
 }
