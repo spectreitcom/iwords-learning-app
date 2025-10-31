433bcc7e53f8a824de3b87e168ed934f5f2010af
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-10-31 10:50:32 +0100

    chore(api): add endpoint for deleting a single repetition by ID in `RepetitionsController`

--- apps/api/src/bff/gateway/controllers/repetitions.controller.ts
+++ apps/api/src/bff/gateway/controllers/repetitions.controller.ts
@@ -3,6 +3,8 @@ import {
   Delete,
   HttpCode,
   HttpStatus,
+  Param,
+  ParseUUIDPipe,
   UseGuards,
 } from '@nestjs/common';
 import { ClerkAuthGuard } from '../auth/clerk-auth.guard';
@@ -31,5 +33,23 @@ export class RepetitionsController {
   @HttpCode(HttpStatus.NO_CONTENT)
   async removeAllRepetitions(@CurrentUserId() userId: string) {
     return await this.repetitionApiService.deleteAllUserRepetitions(userId);
+  }
+
+  @ApiBearerAuth('app-auth')
+  @ApiOperation({ summary: 'Removes a repetition' })
+  @ApiResponse({
+    status: HttpStatus.NO_CONTENT,
+    description: 'Repetition removed successfully',
+  })
+  @Delete(':repetitionId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(
+    @CurrentUserId() userId: string,
+    @Param('repetitionId', new ParseUUIDPipe()) repetitionId: string,
+  ) {
+    return await this.repetitionApiService.deleteOneUserRepetition(
+      userId,
+      repetitionId,
+    );
   }
 }
