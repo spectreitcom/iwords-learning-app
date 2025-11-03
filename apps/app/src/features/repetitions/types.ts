5c739f7256d6e8c64d664b5e70c62c753a0ccbfd
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-11-03 13:12:11 +0100

    chore(app): update `RepetitionExpressionContext` type to use new `RepetitionExpressionContextType`

--- apps/app/src/features/repetitions/types.ts
+++ apps/app/src/features/repetitions/types.ts
@@ -1,11 +1,17 @@
-import { BoxItemType } from "@/features/boxes/types";
+export type RepetitionExpressionContextType =
+  | "verb"
+  | "noun"
+  | "adjective"
+  | "adverb"
+  | "phrasal_verb"
+  | "irregular_verb";
 
 export type RepetitionExpressionContext = {
   expressionContextId: string;
   expressionId: string;
   phrase: string;
   translation: string;
-  type: BoxItemType;
+  type: RepetitionExpressionContextType;
   forms: string[];
   isCountable: boolean;
   isIrregular: boolean;
