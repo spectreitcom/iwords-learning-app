40c34288113ebd2d17ad4b0de84ed1c54c923534
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-11-12 11:49:32 +0100

    chore(app): add `simple_expression` to `expressionTypeMap` and update type usage

--- apps/app/src/features/boxes/utils.ts
+++ apps/app/src/features/boxes/utils.ts
@@ -1,11 +1,12 @@
-import { BoxItemType } from "@/features/boxes/types";
+import { ExpressionContextType } from "@/lib/types";
 
-const expressionTypeMap = new Map<BoxItemType, string>();
+const expressionTypeMap = new Map<ExpressionContextType, string>();
 expressionTypeMap.set("verb", "czasownik");
 expressionTypeMap.set("irregular_verb", "nieregularny czasownik");
 expressionTypeMap.set("noun", "rzeczownik");
 expressionTypeMap.set("adjective", "przymiotnik");
 expressionTypeMap.set("phrasal_verb", "czasownik frazowy");
 expressionTypeMap.set("adverb", "przysłówek");
+expressionTypeMap.set("simple_expression", "proste wyrażenie");
 
 export { expressionTypeMap };
