a03a60f0b136ce4dad7a51c87964735753fe09ba
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2026-01-08 14:20:04 +0100

    chore(api): add OutboxCron for scheduled cleanup of old outbox messages

--- apps/api/src/common/outbox/outbox.module.ts
+++ apps/api/src/common/outbox/outbox.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { OutboxService } from './outbox.service';
 import { OutboxPublisher } from './outbox.publisher';
+import { OutboxCron } from './outbox.cron';
 
 @Module({
   imports: [PrismaModule],
-  providers: [OutboxService, OutboxPublisher],
+  providers: [OutboxService, OutboxPublisher, OutboxCron],
   exports: [OutboxService],
 })
 export class OutboxModule {}
