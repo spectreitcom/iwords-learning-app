cec3c692bb8ec5e9837a5d18e5bdb1b3923487d1
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2026-01-12 18:06:59 +0100

    chore(api): refactor tests and config to use `__test` directory

---
+++ apps/api/src/repetition/application/command-handlers/__tests/delete-one-user-repetition.command-handler.spec.ts
@@ -0,0 +1,42 @@
+import { randomUUID } from 'node:crypto';
+import { FakeRepetitionRepository } from '../../../../../__test/fakes/fake-repetition-repository';
+import { DeleteOneUserRepetitionCommandHandler } from '../delete-one-user-repetition.command-handler';
+
+describe('DeleteOneUserRepetitionCommandHandler', () => {
+  it("it should delete the user's repetition from the list", async () => {
+    const USER_ID = randomUUID();
+    const REPETITION_ID = randomUUID();
+
+    const repetitionRepository = new FakeRepetitionRepository([
+      {
+        id: REPETITION_ID,
+        userId: USER_ID,
+        nextRepetition: new Date(),
+        expressionContextId: randomUUID(),
+      },
+      {
+        id: randomUUID(),
+        userId: USER_ID,
+        nextRepetition: new Date(),
+        expressionContextId: randomUUID(),
+      },
+      {
+        id: randomUUID(),
+        userId: randomUUID(),
+        nextRepetition: new Date(),
+        expressionContextId: randomUUID(),
+      },
+    ]);
+
+    const handler = new DeleteOneUserRepetitionCommandHandler(
+      repetitionRepository,
+    );
+
+    await handler.execute({
+      userId: USER_ID,
+      repetitionId: REPETITION_ID,
+    });
+
+    expect(repetitionRepository.getLength()).toEqual(2);
+  });
+});
