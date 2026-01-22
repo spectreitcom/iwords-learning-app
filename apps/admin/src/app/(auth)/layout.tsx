c55874e71cb1712ae50f9819338c5808ba18f5e2
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2026-01-22 12:08:05 +0100

    chore(ui): remove background color from AuthLayout container

--- apps/admin/src/app/(auth)/layout.tsx
+++ apps/admin/src/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export default function AuthLayout({ children }: PropsWithChildren) {
   return (
-    <div className={"h-screen flex items-center justify-center bg-gray-100"}>
+    <div className={"h-screen flex items-center justify-center"}>
       <div>{children}</div>
     </div>
   );
